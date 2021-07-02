5e03cd50bd710a9e65d0e7b3ea532e7aef30bf4b
김동환 <1033513@naver.com>
2021-07-02 20:40:45 +0900

    Initial Commit

---
+++ FructusMyself/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FructusMyself
+//
+//  Created by 김동환 on 2021/07/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FructusMyself/FructusMyselfApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusMyselfApp.swift
+//  FructusMyself
+//
+//  Created by 김동환 on 2021/07/02.
+//
+
+import SwiftUI
+
+@main
+struct FructusMyselfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
